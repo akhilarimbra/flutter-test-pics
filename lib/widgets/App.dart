632482d482578db2d5f997e4b930437a85230cbc
Akhil Raj Arimbra <nilamburakhil@gmail.com>
2018-07-05 20:10:16 +0530

    added widgets/App

---
+++ lib/widgets/App.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class App extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return AppState();
+  }
+}
+
+class AppState extends State<App> {
+  int counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Let's see some pics"),
+          centerTitle: true,
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              counter++;
+            });
+          },
+          child: Icon(Icons.add),
+        ),
+        drawer: Drawer(),
+        body: Center(
+          child: Text('Pressed ${this.counter} times'),
+        ),
+      ),
+    );
+  }
+}
